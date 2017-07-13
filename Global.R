538d881cf0d3a2f1e190ec485d013d0ab64a9e28
Alcides Chaux <achaux@gmail.com>
2017-07-13 12:02:41 -0400

    EDA v1.1

---
+++ Global.R
@@ -0,0 +1,13 @@
+library(knitr)
+opts_chunk$set(
+  echo=FALSE,
+  warning=FALSE,
+  message=FALSE,
+  fig.align="center"
+)
+
+library(ggplot2)
+library(dplyr)
+library(simpleR)
+
+load("../Data/DF.Rdata")
